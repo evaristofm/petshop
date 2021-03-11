2994fe3fa7399e64993e6c45f2b8d2e00139bd92
evaristofm <evaristofm@gmail.com>
2021-03-11 18:20:47 -0300

    Create app users e views login and logout

--- petshop_py/urls.py
+++ petshop_py/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('pet.urls')),
+    path('pets/', include('pet.urls')),
+    path('users/', include('users.urls')),
 ]
